8ed614e11a84a3e6d2ca37ddd3231bd565f5263f
Luciano Machado <lurmachado@hotmail.com>
2024-03-30 18:25:28 -0300

    Tratamento de Excecoes

---
+++ Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TratamentoExcecoes.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
